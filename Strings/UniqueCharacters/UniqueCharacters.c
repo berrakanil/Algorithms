2093ef88c20e00f0c0b13300480a8a013f34c8c0
berrak <berrak@ubuntu.(none)>
2015-09-09 23:50:13 +0100

    Adding UniqueCharacters C and Java implementation

---
+++ Strings/UniqueCharacters/UniqueCharacters.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "string.h"
+#include <stdbool.h>
+
+/**
+* Assuming charset is ascii
+* Time complexity: O(n)
+* Space complexity: O(1)
+* n is the number of chars in the string
+*/
+bool hasUniqueChars(char* string) {
+	int length = strlen(string);
+	int i = 0, countChar = 0;
+	
+	if (length <= 0) {
+		return false;
+	}
+
+	for (i = 0; i < length; ++i) {
+		int value = string[i] - 'a';
+		if ( (countChar & (1 << value)) == 0) {
+			countChar |= (1 << value);
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
---
+++ Strings/UniqueCharacters/UniqueCharacters.java
@@ -0,0 +1,48 @@
+
+		}ckage com.berrakanil.algorithms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueCharacters {
+
+	/**
+	 * Assuming charset is ascii Time complexity: O(n) Space complexity: O(1) n
+	 * is the number of chars in the string
+	 */
+	public static boolean hasUniqueChars(String str) throws Exception {
+		if (str == null) {
+			throw new Exception("String can not be null");
+		}
+
+		int asciiCounter = 0;
+		for (int i = 0; i < str.length(); ++i) {
+			int value = str.charAt(i) - 'a';
+			if ((asciiCounter & (1 << value)) == 0)
+				asciiCounter |= (1 << value);
+			else
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Time complexity: O(n) Space complexity: O(n) n is the number of chars in
+	 * the string
+	 */
+	public boolean hasUniqueCharsWithSet(String str) throws Exception {
+		if (str == null) {
+			throw new Exception("String can not be null");
+		}
+
+		Set<Character> charSet = new HashSet<Character>();
+		for (char c : str.toCharArray()) {
+			if (charSet.contains(c)) {
+				return false;
+			} else {
+				charSet.add(c);
+			}
+		}
+		return true;
+	}
+}
